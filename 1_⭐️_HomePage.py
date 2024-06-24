4b57c5a02a9256d024a5519727019f4935f040f2
gregdilella <37993510+gregdilella@users.noreply.github.com>
2024-06-23 21:14:13 -0400

    fix

--- 1_⭐️_HomePage.py
+++ 1_⭐️_HomePage.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # from streamlit_gsheets import GSheetsConnection
-from streamlit_lottie import st_lottie
+# from streamlit_lottie import st_lottie
 from streamlit_extras.let_it_rain import rain
 import pandas as pd
 from pathlib import Path
@@ -48,14 +48,14 @@ st.markdown("""
 THIS_DIR = Path(__file__).parent
 CSS_FILE = THIS_DIR / "style" / "style.css"
 ASSETS = THIS_DIR / "assets"
-LOTTIE_ANIMATION = ASSETS / "WhatsAppVideo.json"
+# LOTTIE_ANIMATION = ASSETS / "WhatsAppVideo.json"
 LOGO = ASSETS / "5xUEXG-LogoMakr.png"
 
 
-# Function to load and display the Lottie animation
-def load_lottie_animation(file_path):
-    with open(file_path, "r") as f:
-        return json.load(f)
+# # Function to load and display the Lottie animation
+# def load_lottie_animation(file_path):
+#     with open(file_path, "r") as f:
+#         return json.load(f)
     
 
 with open(CSS_FILE) as f:
@@ -63,7 +63,7 @@ with open(CSS_FILE) as f:
 
 
 
-lottie_animation = load_lottie_animation(LOTTIE_ANIMATION)
+# lottie_animation = load_lottie_animation(LOTTIE_ANIMATION)
 
 col1, col2 = st.columns(2)
 
@@ -153,47 +153,7 @@ st.markdown("""
             """, unsafe_allow_html=True)
 
 # Onboarding New Vendor Form
-# with st.form(key="vendor_form"):
-    
-#     Name = st.text_input(label="Name*")
-#     Attending = st.radio("Are You Planning On Coming?*",[":green[Yes]", ":red[No]"],index=None,horizontal=True)
-#     # Attending = st.selectbox("Are You Planning On Coming?*", options=YES_NO, index=None)
-#     NumberOfParty = st.slider("How Many People Are In Your Party?*", 1, 5, 1)
-#     MealRestriction = st.text_area(label="Do You Have Any Meal Restrictions?    If Yes, Please Describe Them Here.",height=50)
-#     additional_info = st.text_area(label="Additional Info", height=50)
-
-#     # Mark mandatory fields
-#     st.markdown("**required*")
-
-#     submit_button = st.form_submit_button(label="Submit Form")
-
-#     # If the submit button is pressed
-#     if submit_button:
-#         # Check if all mandatory fields are filled
-#         if not Attending or not Name:
-#             st.warning("Ensure all mandatory fields are filled.")
-#             st.stop()
-#         else:
-#             # Create a new row of data
-#             new_data = {
-#                 "Name": Name,
-#                 "Attending": Attending,
-#                 "Number": NumberOfParty,
-#                 "Meal": MealRestriction,
-#                 "Notes": additional_info,
-#                 "Time": datetime.datetime.now()  # Assign the timestamp only here
-#             }
-
-#             # Add the new data to the existing DataFrame
-#             updated_df = existing_data.append(new_data, ignore_index=True)
-
-#             # Update Google Sheets with the new data
-#             conn.update(worksheet="Responses", data=updated_df)
-
-#             if Attending == ":red[No]":
-#                 st.warning(f"Sorry you can't make it, {Name}!")
-#             elif Attending == ":green[Yes]":
-#                 st.success(f"Looking forward to seeing you there, {Name}!")
+# source venv/bin/activate
 
 
 st.markdown("""
